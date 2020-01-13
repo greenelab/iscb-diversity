b3c6198206023e966bbafa70ed649bc2ffd2e21c
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2020-01-13 12:27:42 -0500

    convert jupyter notebooks

--- docs/readme.md
+++ docs/readme.md
@@ -8,6 +8,13 @@ More information at <https://github.com/greenelab/iscb-diversity>.
 
 See the following rendered notebooks:
 
+- [`01.download-pubmed.html`](01.download-pubmed.html)
+- [`02.parse-pubmed.html`](02.parse-pubmed.html)
+- [`03.download-pmc.html`](03.download-pmc.html)
+- [`04.process-pmc.html`](04.process-pmc.html)
+- [`05.download-iscb.html`](05.download-iscb.html)
+- [`06.combine-names.html`](06.combine-names.html)
+- [`07.genderize.html`](07.genderize.html)
 - [`08.visualize-gender.html`](08.visualize-gender.html)
 - [`09.predict-race.html`](09.predict-race.html)
 
--- utils/prepare_docs.py
+++ utils/prepare_docs.py
@@ -2,8 +2,12 @@
 Generate docs/readme.me for GitHub Pages.
 """
 import pathlib
+import subprocess
 
-readme_template = '''\
+root_dir = pathlib.Path(__file__).parent.parent
+docs_dir = root_dir.joinpath("docs")
+
+readme_template = """\
 # Project Webpage for ISCB Diversity Analysis
 
 <!-- make sure to edit this content in utils/prepare_docs.py and not docs/readme.md -->
@@ -15,15 +19,34 @@ More information at <https://github.com/greenelab/iscb-diversity>.
 See the following rendered notebooks:
 
 {notebook_list_md}
-'''
+"""
 
-if __name__ == "__main__":
-    docs_dir = pathlib.Path(__file__).parent.parent.joinpath('docs')
-    assert docs_dir.is_dir()
+
+def render_jupyter_notebooks():
+    ipynb_paths = sorted(root_dir.glob("*.ipynb"))
+    ipynb_paths = [path.relative_to(root_dir) for path in ipynb_paths]
+    args = [
+        "jupyter",
+        "nbconvert",
+        "--output-dir=docs",
+        *ipynb_paths,
+    ]
+    subprocess.run(args, cwd=root_dir)
+
+
+def get_notebook_list_md():
     html_paths = sorted(docs_dir.glob("**/*.html"))
     notebook_list_md = ""
     for path in html_paths:
         path = path.relative_to(docs_dir)
         notebook_list_md += f"- [`{path}`]({path})\n"
+    return notebook_list_md
+
+
+if __name__ == "__main__":
+    assert docs_dir.is_dir()
+    render_jupyter_notebooks()
+    html_paths = sorted(docs_dir.glob("**/*.html"))
+    notebook_list_md = get_notebook_list_md()
     readme = readme_template.format(notebook_list_md=notebook_list_md)
     docs_dir.joinpath("readme.md").write_text(readme)
