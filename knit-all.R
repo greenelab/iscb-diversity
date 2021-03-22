a61d67806a5212488b4c574078f80467cbd8cf5d
lelaboratoire <grixor@gmail.com>
2021-03-22 10:32:35 -0400

    update regression

--- knit-all.R
+++ knit-all.R
@@ -1,10 +1,16 @@
-rnotebooks <- list.files(path = ".", pattern = '.Rmd')
+# local_path <- '.' # without docker
+local_path <- 'repo' # with docker
+
+# on docker:
+rnotebooks <- list.files(path = local_path, 
+                         pattern = '.Rmd',
+                         full.names = TRUE)
 
 for (notebooki in rnotebooks){
   print(notebooki)
   rmarkdown::render(notebooki, 
-                    output_dir = 'docs',
+                    output_dir = file.path(local_path, 'docs'),
                     output_yaml = '_output.yaml')  
 }
 
-beepr::beep()
+# beepr::beep()
